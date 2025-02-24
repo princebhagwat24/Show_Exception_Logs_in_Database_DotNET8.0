1f8b2019bc2dbc8f10b6b375f3ca7710dc32f823
Prince Bhagwat <104710793+princebhagwat24@users.noreply.github.com>
2025-02-24 14:17:57 +0530

    Add files via upload

---
+++ Models/ErrorLog.cs
@@ -0,0 +1,12 @@
+﻿namespace ExceptionLogsTask.Models
+{
+    public class ErrorLog
+    {
+        public int Id { get; set; }
+        public string ControllerName { get; set; }
+        public string ActionName { get; set; }
+        public string ExceptionMessage { get; set; }
+        public string StackTrace { get; set; }
+        public DateTime LogDate { get; set; }
+    }
+}
